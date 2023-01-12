ab56f0c233ff1382898ffbf715dcd730923824fe
Norberto <norgarron@gmail.com>
2023-01-12 16:38:05 -0600

    javascript semana 5

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+let nombreUsuario = prompt("Hola como te llamas ")
+const saludo = alert ("hola " + nombreUsuario)
+console.log (nombreUsuario)
+
+alert ("costo de playera 25 pesos")
+const playeras = parseInt(prompt ("¿cuantas playeras quieres impresas?"))
+console.log (playeras)
+
+alert ("total a pagar " + playeras * 25 + " pesos")
+const saludos = "Listo para crear";
+console.log(saludos);
+alert (saludos)
+
+
---
+++ scripts/productos.js
@@ -0,0 +1,20 @@
+if (select  = "serigrafia") {
+    alert ("La serigrafía consiste en transferir el logo o la imagen que se desea estampar a través de una malla denominada pantalla. Por cada color que se quiera, necesitamos una pantalla adicional. Estas pantallas permiten pasar la tinta para formar el dibujo deseado.");
+}else{
+    alert ("elige una opcion")
+}
+if (select = "corte vinil") {
+    alert ("El Corte en Vinil es un sistema de presentación gráfica que involucra un Plotter y al vinil. Aunque éste no se limita al corte estricto y permite elaborar suajes precisos en trabajos de impresión. El corte de vinil es la alternativa ideal para señalizar, anunciar, elaborar carteles, cortar stickers, entre otras más.");
+}else{
+    alert ("elige una opcion")
+}
+if (select = "diseño digital") {
+    alert ("El diseño digital es un término que engloba cualquier forma de comunicación visual y contenido que utilice una interfaz digital para ofrecer información, un producto o un servicio a quien tenga la suerte de verlo.");
+}else{
+    alert ("elige una opcion")
+}
+if (select = "sublimado") {
+    alert ("Lo que llamamos sublimación textil es una técnica que se utiliza mayoritariamente para personalizar o imprimir prendas blancas de poliéster 100%. Es el sistema de impresión ideal para las prendas técnicas ya que permite la transpiración. El calor normalmente se aplica con una prensa o plancha térmica.");
+}else{
+    alert ("elige una opcion")
+}
